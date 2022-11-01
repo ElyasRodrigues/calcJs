decfd156c3a844dd061938e9798c40a3fca01cb1
Elyas Rodrigues <elyasrz11@gmail.com>
2022-11-01 18:22:08 -0300

    finish project

--- index.js
+++ index.js
@@ -47,14 +47,14 @@ document.getElementById("themeSwitcher").addEventListener("click", () => {
   if(main.dataset.theme === "dark"){
     root.style.setProperty("--bg-color", "#f1f5f9")
     root.style.setProperty("--border-color", "#aaa")
-    root.style.setProperty("--font-color", "#212529")
+    root.style.setProperty("--font-color", "#0e1822")
     root.style.setProperty("--primary-color", "#26834a")
     main.dataset.theme = "light"
   } else {
-    root.style.setProperty("--bg-color", "#212529")
+    root.style.setProperty("--bg-color", "#0e1822")
     root.style.setProperty("--border-color", "#666")
     root.style.setProperty("--font-color", "#f1f5f9")
-    root.style.setProperty("--primary-color", "#4dff91")
+    root.style.setProperty("--primary-color", "#15f56b")
     main.dataset.theme = "dark"
   }
 })
